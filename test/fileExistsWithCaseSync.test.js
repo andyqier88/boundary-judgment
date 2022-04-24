b0fb8d46013c2000dfd8a40e9e07ed7ddda47c75
王英琦 <wangyingqi@chinapost.com.cn>
2022-04-24 11:35:45 +0800

    fix: jest test

--- test/fileExistsWithCaseSync.test.js
+++ test/fileExistsWithCaseSync.test.js
@@ -1,6 +1,12 @@
-import fileExistsWithCaseSync from './../lib/fileExistsWithCaseSync/fileExistsWithCaseSync'
+import fileExistsWithCaseSync from './../lib/fileExistsWithCaseSync/fileExistsWithCaseSync.js'
+
 
 
 test('fileExistsWithCaseSync', () => {
-    expect(fileExistsWithCaseSync('./../README.md')).toBe(true);
-},1000);
+    const isExists = fileExistsWithCaseSync('./README.md')
+    expect(isExists).toBe(true)
+});
+test('fileExistsWithCaseSync case-sensitive', () => {
+    const isExistsWithCase = fileExistsWithCaseSync('./readme.md')
+    expect(isExistsWithCase).toBe(false)
+});
